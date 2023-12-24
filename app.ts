5bb7018e206e8e11b4cf32d981ee45c5d6ff0c4d
Tirath Sharma <tirathsharma098@gmail.com>
2023-12-24 20:12:40 +0530

    fixed bug getting chat twice

--- app.ts
+++ app.ts
@@ -16,6 +16,20 @@ const PORT = process.env.PORT;
 const app: Application = express();
 // Setting cors
 app.use(cors());
+// app.all("*", function (req, res, next) {
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "X-Requested-With, content-type, Authorization, Accept"
+//     );
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Expose-Headers", "Authorization");
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     next();
+// });
 app.all("*", settingHeader);
 app.use(helmet());
 app.use(express.json());
--- src/socket/connection.ts
+++ src/socket/connection.ts
@@ -3,22 +3,21 @@ export const socketConnection = io => {
         console.log(">> CONNECTED TO SOCKET IO");
         // socket.broadcast.emit("t_c", { a_u: totalUsers });
 
-        socket.on("setup", userData => {
-            socket.join(userData);
+        socket.on("setup", () => {
             socket.emit("connected");
         });
 
         socket.on("join_chat", chat_id => {
-            // console.log(socket.rooms, socket.id)
-            socket.rooms.forEach(room => {
-                // console.log( "room got", room)
-                socket.leave(room);
-            })
+            console.log(socket.rooms, socket.id)
             socket.join(chat_id);
             socket.emit("chat_connected", chat_id);
         });
+        socket.on("leave_chat", chat_id => {
+            console.log(">>> leaving chat : ", chat_id)
+            socket.leave(chat_id)
+        })
         socket.on("private_message", (message, room_id) => {
-            socket.to(room_id).emit("private_message_received", message);
+            socket.to(room_id).emit("private_message_received", message, room_id);
         });
         socket.on("disconnect", () => {
             console.log("USER DISCONNECTED");
